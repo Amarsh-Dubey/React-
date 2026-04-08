55d1207df08f271375fbf60efbc384a0cf8856e9
Amarsh-Dubey <nimeparadise@gmai.com>
2026-04-08 12:08:21 +0530

    Inlined css

--- src/Component/app.jsx
+++ src/Component/app.jsx
@@ -1,81 +1,105 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-// class MyComponent extends Component {
+// // class MyComponent extends Component {
 
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       count: 0,
-//       data: null,
-//       loading: true,
-//     };
+// //   constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //       count: 0,
+// //       data: null,
+// //       loading: true,
+// //     };
 
-//     this.handleClick = this.handleClick.bind(this);
-//   }
+// //     this.handleClick = this.handleClick.bind(this);
+// //   }
 
-//   componentDidMount() {
-//     fetch('https://jsonplaceholder.typicode.com/posts/1')
-//       .then(res => res.json())
-//       .then(data => {
-//         this.setState({ data, loading: false });
-//       });
+// //   componentDidMount() {
+// //     fetch('https://jsonplaceholder.typicode.com/posts/1')
+// //       .then(res => res.json())
+// //       .then(data => {
+// //         this.setState({ data, loading: false });
+// //       });
 
-//     this.timer = setInterval(() => {
-//       this.setState(prev => ({ count: prev.count + 1 }));
-//     }, 1000);
-//   }
+// //     this.timer = setInterval(() => {
+// //       this.setState(prev => ({ count: prev.count + 1 }));
+// //     }, 1000);
+// //   }
 
-//   componentDidUpdate(prevProps, prevState) {
-//     if (prevState.count !== this.state.count) {
-//       console.log('Count changed to:', this.state.count);
+// //   componentDidUpdate(prevProps, prevState) {
+// //     if (prevState.count !== this.state.count) {
+// //       console.log('Count changed to:', this.state.count);
+// //     }
+// //   }
+
+// //   componentWillUnmount() {
+// //     clearInterval(this.timer);
+// //   }
+
+// //   handleClick() {
+// //     this.setState(prev => ({ count: prev.count + 1 }));
+// //   }
+
+// //   render() {
+// //     const { count, data, loading } = this.state;
+
+// //     return (
+// //       <div>
+// //         <h1>Count: {count}</h1>
+// //         <button onClick={this.handleClick}>Increment</button>
+
+// //         {loading ? (
+// //           <p>Loading...</p>
+// //         ) : (
+// //           <p>Data: {data?.title}</p>
+// //         )}
+// //       </div>
+// //     );
+// //   }
+// // }
+
+// // export default MyComponent;
+// class sample extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             count: 0,
+//         }
 //     }
-//   }
+//     componentDidMount() {
+//         console.log('Component mounted');
+//     }
 
-//   componentWillUnmount() {
-//     clearInterval(this.timer);
-//   }
+//     shouldComponentUpdate(nextProps, nextState) {
+//     //     if (nextState.count > 5) {
+//     //         return false;
+//     //     }
+//     //     return true;
+//     // }
+//     console.log('Should component update?');
+//     return true;
+//     }
+import React from 'react'
+import { useState } from 'react'
 
-//   handleClick() {
-//     this.setState(prev => ({ count: prev.count + 1 }));
-//   }
-
-//   render() {
-//     const { count, data, loading } = this.state;
-
-//     return (
-//       <div>
-//         <h1>Count: {count}</h1>
-//         <button onClick={this.handleClick}>Increment</button>
-
-//         {loading ? (
-//           <p>Loading...</p>
-//         ) : (
-//           <p>Data: {data?.title}</p>
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default MyComponent;
-class sample extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0,
-        }
-    }
-    componentDidMount() {
-        console.log('Component mounted');
+function Sample() {
+  // let count=0
+  const [count, setCount]=useState(0)
+  
+  const increment = () => {
+       setCount(count + 1)
     }
 
-    shouldComponentUpdate(nextProps, nextState) {
-    //     if (nextState.count > 5) {
-    //         return false;
-    //     }
-    //     return true;
-    // }
-    console.log('Should component update?');
-    return true;
+    const decrement = () => {
+       setCount(count - 1)
     }
-    
+  return (
+    <div>
+      <h1>This is a sample component</h1>
+      <h2>{count}</h2>
+      <button style={{backgroundColor:"red",color:"white", width:"100px", height:"30px"}} onClick={increment}>Increment</button>
+      <button style={{backgroundColor:"yellow", color:"black", width:"100px", height:"30px"}} onClick={decrement}>decrement</button>
+    </div>
+  )
+}
+
+export default Sample
