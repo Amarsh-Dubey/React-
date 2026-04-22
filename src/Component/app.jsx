5da0834611f3f7088617e8a3f27d7bb43283cbc4
Amarsh-Dubey <nimeparadise@gmai.com>
2026-04-22 11:37:23 +0530

    lecture 30

--- src/Component/app.jsx
+++ src/Component/app.jsx
@@ -1,114 +1,100 @@
-// import React, { Component } from 'react';
+// import React, { Component } from 'react'
 
-// // class MyComponent extends Component {
-
-// //   constructor(props) {
-// //     super(props);
-// //     this.state = {
-// //       count: 0,
-// //       data: null,
-// //       loading: true,
-// //     };
-
-// //     this.handleClick = this.handleClick.bind(this);
-// //   }
-
-// //   componentDidMount() {
-// //     fetch('https://jsonplaceholder.typicode.com/posts/1')
-// //       .then(res => res.json())
-// //       .then(data => {
-// //         this.setState({ data, loading: false });
-// //       });
-
-// //     this.timer = setInterval(() => {
-// //       this.setState(prev => ({ count: prev.count + 1 }));
-// //     }, 1000);
-// //   }
-
-// //   componentDidUpdate(prevProps, prevState) {
-// //     if (prevState.count !== this.state.count) {
-// //       console.log('Count changed to:', this.state.count);
-// //     }
-// //   }
-
-// //   componentWillUnmount() {
-// //     clearInterval(this.timer);
-// //   }
-
-// //   handleClick() {
-// //     this.setState(prev => ({ count: prev.count + 1 }));
-// //   }
-
-// //   render() {
-// //     const { count, data, loading } = this.state;
-
-// //     return (
-// //       <div>
-// //         <h1>Count: {count}</h1>
-// //         <button onClick={this.handleClick}>Increment</button>
-
-// //         {loading ? (
-// //           <p>Loading...</p>
-// //         ) : (
-// //           <p>Data: {data?.title}</p>
-// //         )}
-// //       </div>
-// //     );
-// //   }
-// // }
-
-// // export default MyComponent;
-// class sample extends Component {
-//     constructor(props) {
-//         super(props);
+//  class Sample extends Component {
+//     constructor(props){
+//         super(props)
 //         this.state = {
 //             count: 0,
 //         }
 //     }
-//     componentDidMount() {
-//         console.log('Component mounted');
+
+//     componentDidMount(){
+//       console.log("Component is mounted")
 //     }
 
-//     shouldComponentUpdate(nextProps, nextState) {
-//     //     if (nextState.count > 5) {
-//     //         return false;
-//     //     }
-//     //     return true;
-//     // }
-//     console.log('Should component update?');
-//     return true;
-//     }
-// import React from 'react'
-// import { useState } from 'react'
-
-// function Sample() {
-//   // let count=0
-//   const [count, setCount]=useState(0)
-  
-//   const increment = () => {
-//        setCount(count + 1)
+//     shouldComponentUpdate(nextProps, nextState){
+//         // if(nextState.count === 1){
+//         //   return false
+//         // }
+//         // return true
+//         console.log("Component should update")
+//         return true
 //     }
 
-//     const decrement = () => {
-//        setCount(count - 1)
+//     componentDidUpdate(){
+//       console.log("Component Did Update")
 //     }
-//   return (
-//     <div>
-//       <h1>This is a sample component</h1>
-//       <h2>{count}</h2>
-//       <button style={{backgroundColor:"red",color:"white", width:"100px", height:"30px"}} onClick={increment}>Increment</button>
-//       <button style={{backgroundColor:"yellow", color:"black", width:"100px", height:"30px"}} onClick={decrement}>decrement</button>
-//     </div>
-//   )
+
+//     componentWillUnmount(){
+//       console.log("Component Will Unmount")
+//     }
+
+//     increment = () => {
+//         this.setState({count: this.state.count + 1})
+//     }
+//   render() {
+//     return (
+//       <div>
+//         <h1>This is a sample component</h1>
+//         <h2>{this.state.count}</h2>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     )
+//   }
 // }
 
 // export default Sample
-import { useState } from 'react'
-import styles from '../Css/app.module.css'
+
+
+import React from 'react'
+import { useState, useEffect } from 'react'
+import styles from "../css/Sample.module.css"
+import NavStyle from "../css/NavBar.module.css"
+// import styled from "styled-components"
+import styled from "@emotion/styled"
+import { css } from '@emotion/react'
+import axios from "axios"
 
 function Sample() {
   // let count=0
   const [count, setCount]=useState(0)
+  const[name, setName]= useState("Devendra")
+  const [loading, setLoading]=useState(false)
+
+  useEffect(()=>{
+    console.log("Component is mounted")
+    return () => {
+      console.log("Component unmount")
+    }
+  },[name])
+
+  useEffect(()=>{
+    // const fetchData = async() => {
+    //   try{
+    //     const response = await fetch("https://jsonplaceholder.typicode.com/todos/500",{
+    //       method: "GET"
+    //     })
+    //     const json = await response.json()
+    //     console.log(json)
+    //   }catch(e){
+    //     console.log(e)
+    //   }
+    // }
+
+    const fetchData = async() => {
+      try{
+        setLoading(true)
+        const response = await axios.get("https://jsonplaceholder.typicode.com/todos/5")
+        console.log(response.data)
+        setLoading(false)
+      }catch(e){
+        
+          console.log(e)
+      }
+    }
+
+    fetchData()
+  },[])
   
   const increment = () => {
        setCount(count + 1)
@@ -121,10 +107,29 @@ function Sample() {
     <div>
       <h1>This is a sample component</h1>
       <h2>{count}</h2>
-      <button className={styles.btn} onClick={increment}>Increment</button>
-      <button className={styles.btn} onClick={decrement}>decrement</button>
+      <h3>{name}</h3>
+      {loading?<h2>Loading...</h2>:""}
+      <button onClick={()=>setName("Alex")}>Change the Name</button>
+      <button css={funcButton} onClick={increment}>Increment</button>
+      <Button danger onClick={decrement}>decrement</Button>
+      <button className='bg-blue-600 text-white w-18'>Sample </button>
     </div>
   )
 }
 
 export default Sample
+
+const funcButton=css`
+    background-color: yellow;
+    color: white;
+    width: 100px;
+    height: 30px;
+    border-radius: 10px;
+  ` 
+const Button=styled.button`
+    background-color: ${props=>props.danger ? "red" : "green"};
+    color: white;
+    width: 100px;
+    height: 30px;
+    border-radius: 10px;
+  `
